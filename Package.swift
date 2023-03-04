47e14350b21fe720695078d48a698c6fe4d35cf4
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-03-04 18:31:17 +0100

    Add Swift package definition

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DDGSyncCrypto",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "DDGSyncCrypto",
+            targets: ["DDGSyncCrypto"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Clibsodium",
+            path: "native_lib/third-party/apple/Clibsodium.xcframework"
+        ),
+        .target(
+            name: "DDGSyncCrypto",
+            dependencies: ["Clibsodium"],
+            path: "native_lib",
+            exclude: ["native_lib/third-party"]
+        )
+    ]
+)
