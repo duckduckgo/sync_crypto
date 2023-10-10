ac94e06c7e2968c251bdd3fd9f22100340a0efc9
David González <malmstein@gmail.com>
2023-10-10 16:53:03 +0200

    updated scripts

--- test/script.sh
+++ test/script.sh
@@ -1,8 +1,2 @@
-curl -O- https://github.com/duckduckgo/sync-config/blob/lkruger/sync1/test/setup.sh
-curl -O- https://github.com/duckduckgo/sync-config/blob/lkruger/sync1/test/sync_predefined_autofill.json
-curl -O- https://github.com/duckduckgo/sync-config/blob/lkruger/sync1/test/sync_predefined_bookmarks.json
-curl -O- https://github.com/duckduckgo/sync-config/blob/lkruger/sync1/test/test_user_setup
-curl -O- https://github.com/duckduckgo/sync-config/blob/lkruger/sync1/test/teardown.sh
-
 json="$(./setup.sh)"
 echo "$json"
